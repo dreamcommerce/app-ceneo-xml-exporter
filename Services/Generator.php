0af6338de679a3203f54cc6ee37b3a93afa48652
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2015-04-10 12:11:54 +0200

    product category

--- Services/Generator.php
+++ Services/Generator.php
@@ -11,8 +11,11 @@ namespace CeneoBundle\Services;
 
 use CeneoBundle\Manager\ExcludedProductManager;
 use DreamCommerce\Client;
+use DreamCommerce\Resource\CategoriesTree;
+use DreamCommerce\Resource\Category;
 use DreamCommerce\Resource\Product;
 use DreamCommerce\ShopAppstoreBundle\Model\ShopInterface;
+use DreamCommerce\ShopAppstoreBundle\Utils\CollectionWrapper;
 use DreamCommerce\ShopAppstoreBundle\Utils\Fetcher;
 
 class Generator {
@@ -33,6 +36,9 @@ class Generator {
     protected $client;
 
     protected $count = 0;
+
+    protected $categories;
+    protected $categoriesTree;
 
     function __construct($output, Client $client, ExcludedProductManager $excludedProductManager)
     {
@@ -59,6 +65,8 @@ class Generator {
 
         $fetcher = new Fetcher($productResource);
 
+        $this->loadCategories();
+
         $w->startDocument();
             $w->startElementNs('xsi', 'offers', 'http://www.w3.org/2001/XMLSchema');
             $w->writeAttribute('version', 1);
@@ -80,12 +88,68 @@ class Generator {
 
         $this->count++;
 
+        $categoryPath = $this->getCategoryPath($row->category_id);
+
         $w = $this->resource;
         $w->startElement('o');
             $w->writeAttribute('id', $row->product_id);
             $w->startElement('name');
                 $w->writeCdata($row->translations->pl_PL->name);
             $w->endElement();
+            $w->startElement('cat');
+                $w->writeCdata($categoryPath);
+            $w->endElement();
         $w->endElement();
+    }
+
+    protected function loadCategories(){
+        $categoriesTreeResource = new CategoriesTree($this->client);
+        $categoriesResource = new Category($this->client);
+
+        $this->categoriesTree = $categoriesTreeResource->get();
+
+        $fetcher = new Fetcher($categoriesResource);
+        $categories = $fetcher->fetchAll();
+        $wrapper = new CollectionWrapper($categories);
+
+        $this->categories = $wrapper->getArray('category_id');
+    }
+
+    protected function getCategoryPath($id){
+
+        static $cache;
+
+        if(isset($cache[$id])){
+            return $cache[$id];
+        }
+
+        $targetPath = array();
+
+        $iterator = function($node, $path) use (&$targetPath, $id, &$iterator){
+
+            foreach($node as $i){
+                if($i['id']==$id){
+                    $path[] = $id;
+                    $targetPath = $path;
+                    return;
+                }else{
+                    $path[] = $i['id'];
+                    $iterator($i['children'], $path);
+                }
+            }
+
+        };
+
+        $iterator($this->categoriesTree, array());
+
+        foreach($targetPath as &$n){
+            $n = $this->categories[$n]->translations->pl_PL->name;
+        }
+
+        $stringPath = implode('/', $targetPath);
+        $cache[$id] = $stringPath;
+
+        return $stringPath;
+
     }
 }
