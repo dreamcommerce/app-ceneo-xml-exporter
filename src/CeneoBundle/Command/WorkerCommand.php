3cb989613ae9d9bd1cd14f57ce8dc66f5555d118
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2015-10-27 11:24:36 +0100

    worker alias

---
+++ src/CeneoBundle/Command/WorkerCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CeneoBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WorkerCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('ceneo:worker')
+            ->setDescription('Executes export worker');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $command = $this->getApplication()->find('gearman:worker:execute');
+        $arguments = array(
+            'command' => 'gearman:worker:execute',
+            'worker'    => 'CeneoBundleWorkerGeneratorWorker',
+            '-n'  => true,
+        );
+
+        $input = new ArrayInput($arguments);
+
+        $command->run($input, $output);
+    }
+}
