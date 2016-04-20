cc78cd06c8ea9bc3729fec9ddffe655ef023e8a8
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2016-04-20 12:31:01 +0200

    DOK-220 - rounding stock value

--- src/CeneoBundle/Services/Generator.php
+++ src/CeneoBundle/Services/Generator.php
@@ -441,11 +441,13 @@ class Generator {
 
         $permalink = 'http://'.$permalink;
 
+        $stock = round($row->stock->stock);
+
         $w = $writer;
         $w->startElement('o');
             $w->writeAttribute('id', $row->product_id);
             $w->writeAttribute('price', $row->stock->comp_promo_price);
-            $w->writeAttribute('stock', $row->stock->stock);
+            $w->writeAttribute('stock', $stock);
             $w->writeAttribute('url', $permalink);
             $w->writeAttribute('weight', $row->stock->weight);
             $w->writeAttribute('avail', $this->getDaysForDeliveryId($row->stock->delivery_id));
